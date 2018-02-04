adf3d61fc44607922b9e83dd681007100f713fb4
Daloupe <jesse@circuitbentobox.com>
2018-02-04 05:59:07 +0000

    Seperate Client and host.

---
+++ Comads/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Runtime;
+
+namespace Comads
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            while (true)
+            {
+                try
+                {
+                    var client = ClientBuilder
+                    .CreateDefault()
+                    .LoadConfiguration("ClientConfiguration.xml")
+                    .ConfigureServices(services =>
+                    {
+                      // Add services if you ddarrreeeeee
+                    })
+                    .Build();
+
+                    await client.Connect().ConfigureAwait(false);
+                    Console.WriteLine("Connected to silo!");
+
+                    var friend = client.GetGrain<IGrain1>(0);
+                    Console.WriteLine(await friend.Reply("Hello!"));
+
+
+                    Console.ReadLine();
+                    break;
+                }
+                catch (SiloUnavailableException)
+                {
+                    Console.WriteLine("Silo not available! Retrying in 3 seconds.");
+                    Thread.Sleep(3000);
+                }
+            }
+        }
+    }
+}
